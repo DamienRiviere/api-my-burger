80149ec7a3beefd2957579148eefc298e5fedd9e
Damien Riviere <riviere.damien64@hotmail.com>
2020-10-09 15:05:24 +0200

    ci(CircleCI): Delete run command for behat and phpunit in config.yml.

--- src/Action/NewUser.php
+++ src/Action/NewUser.php
@@ -35,9 +35,6 @@ final class NewUser
     /** @var SerializerInterface */
     protected $serializer;
 
-    /** @var UserRepository */
-    protected $userRepository;
-
     /**
      * NewUser constructor.
      * @param EntityManagerInterface $em
